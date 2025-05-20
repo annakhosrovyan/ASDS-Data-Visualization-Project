d993d0d072672bfa3d2c39bc2e5b24263f92c07e
annakhosrovyan <anna.khosrovyan.1806@gmail.com>
2025-05-20 19:28:01 +0400

    update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dash import Dash, html, page_container
 import dash_bootstrap_components as dbc
 
 app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
 
 navbar = dbc.NavbarSimple(
     brand="🧠 Mental Health Dashboard",
